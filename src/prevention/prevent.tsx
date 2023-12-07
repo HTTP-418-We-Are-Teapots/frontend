e1e926aa761273e422830046e160c806507e87b4
math-x <97481289+math-x-io@users.noreply.github.com>
2023-12-07 21:58:59 +0100

    extend map

--- src/prevention/prevent.tsx
+++ src/prevention/prevent.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import './prevent.css'
-// import './prevent.scss' TODO
-const prevent = () => {
-  return (
-<body>
-    <header>
-        <h1 className='prevent-title'></h1>
-    
-    </header>
-        <div className='content'>
-            <p className='text-content'></p>
-            <img src="https://picsum.photos/200/300" alt="" />
-        </div>
 
-        <div className='content-2'>
-            <p className='text-content'></p>
-            <img src="https://picsum.photos/300/400" alt="" />
+import  styles from './prevent.module.scss';
+
+interface PreventProps {
+    title: string
+    contents: string[]
+    imgs: string[]
+}
+
+
+const Prevent = (
+    {
+        title,
+        contents,
+        imgs
+    }: PreventProps
+    ) => {
+return (
+<body className={styles.preventBody}>
+    <header>
+    </header>
+    <div className={styles.preventDisplay}>
+
+    <h1 className={styles.preventTitle}>{title}</h1>
+    {contents.map((content, index) => (
+        <div key={index} className={styles.preventContent}>
+            <p>{content}</p>
+            <img className={styles.preventImg} src={imgs[index]} alt="image" />
         </div>
+        ))}
+    </div>
 </body>
 )
 }
 
-export default prevent
+export default Prevent
