c07f0bdacc202938e59bf9094b421ad840cf76c0
Alexandre Boin <contact@alexandre-boin.fr>
2023-12-07 23:10:19 +0100

    :construction: Router draft

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
