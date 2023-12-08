5e8bdf10c03523ee3667fd809e812ab7920e3135
Killian Mannarelli <kmannarelli@ensc.fr>
2023-12-08 05:55:19 +0100

    Framer + Overlay ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import './App.css'
-import Header from "./components/landing/Header.tsx";
+
 
 import './App.css'
-import Prevent from './prevention/prevent'
 function App() {
 
-const sectionsData = [
-  {
-      title: 'First Title',
-      description: 'This is the first description.',
-      image: "./assets/react.svg"
-  },
-  {
-      title: 'Second Title',
-      description: 'This is the second description.',
-      image: "./assets/react.svg"
-  },
-  // Add more sections as needed
-];
+
   return (
-    <div className='container'>
-        <Header />
-        <Prevent sections={sectionsData} />
+    <div>
+     
         
     </div>
 
