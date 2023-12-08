bca98dd0f75a6ea4b320482ef1ef533f8c272c00
Killian Mannarelli <kmannarelli@ensc.fr>
2023-12-08 03:32:40 +0100

    Framer + Overlay ok

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,16 +1,44 @@
-import '../assets/style/games.scss'
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom'; // If you're using React Router for navigation
+import '../assets/style/games.scss';
 
 const Games = () => {
+    const navigate = useNavigate();
+
+    const games = [
+        { name: 'Card 1', url: '/game1' },
+        { name: 'Card 2', url: '/game2' },
+        { name: 'Card 3', url: '/game3' },
+        { name: 'Card 4', url: '/game4' },
+    ];
+
+    const hoverEffect = {
+        scale: 1.05,
+        boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.2)"
+    };
+
+    const handleClick = (url : string) => {
+        navigate(url); // Navigate to the game's page
+    };
+
     return (
         <div>
             <div className="cards">
-                <div className="card">Card 1</div>
-                <div className="card">Card 2</div>
-                <div className="card">Card 3</div>
-                <div className="card">Card 4</div>
+                {games.map(game => (
+                    <motion.div 
+                        key={game.name}
+                        className="card" 
+                        whileHover={hoverEffect} 
+                        onClick={() => handleClick(game.url)}
+                    >
+                        <p>
+                        {game.name}
+                        </p>
+                    </motion.div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Games
+export default Games;
